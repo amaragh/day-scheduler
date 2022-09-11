77d51b2ed6aaf6918cb13239649998fdbf82a660
amaragh <mercer.alecia@gmail.com>
2022-09-11 14:39:56 -0500

    correct hour comparison for time block colour

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,26 +3,26 @@ var headerDate = moment().format("dddd, MMMM Do");
 
 $("#currentDay").text(headerDate)
 
-var hourChunks = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+// var hourChunks = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+
+var hourChunks = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
 tasks = [];
 
 var displayHourChunks = function () {
-    for (var i = 0; i < hourChunks.length; i++) {
-        var hourChunk = hourChunks[i];
+    for (var x = 0; x < hourChunks.length; x++) {
+        var hourChunk = hourChunks[x];
+        hourChunkFormatted = moment().set("hour", hourChunk).format("hA");
         var timeBlock = $("<div>").addClass("row time-block");
 
-        
-
-        var hourDiv = $("<div>").addClass("hour col-1").text(hourChunk);
+        var hourDiv = $("<div>").addClass("hour col-1").text(hourChunkFormatted);
 
 
-        var taskArea = $("<textarea>").addClass("col-10").attr("id", hourChunk);
-        var relTime = checkTime(taskArea);
+        var taskArea = $("<textarea>").addClass("col-10").attr("id", hourChunkFormatted);
+        var relTime = checkTime(hourChunk);
         var taskArea = taskArea.addClass(relTime);
 
-
-        var save = $("<div>").addClass("saveBtn col-1").append("<button>");
+        var save = $("<div>").addClass("saveBtn col-1").append("<button><i class='far fa-save'></i></button>");
 
         // add timeblock to the .container element
         timeBlock.append(hourDiv, taskArea, save);
@@ -32,16 +32,11 @@ var displayHourChunks = function () {
 
 var checkTime = function (timeBlockEl) {
 
-    var blockTime = $(timeBlockEl).attr("id");
-    var currTime = moment().format("hA");
-
-
-    // var blockTime = moment().format("dddd") + " " + $(timeBlockEl).attr("id");
-    
+    var blockTime = moment().set("hour", timeBlockEl).format("HH");
     // extract hour from current time in format that matches format from the time block
-    // var currTime = moment().format("dddd") + " " +  moment().format("hA");
+    var currTime = moment().format("HH");
 
-    console.log("BLOCK TIME:",blockTime,"; CURR TIME:",currTime);
+    console.log("BLOCK TIME:", blockTime, "; CURR TIME:", currTime);
 
     // compare time for the time block to the current moment
     if (currTime > blockTime) {
@@ -50,7 +45,7 @@ var checkTime = function (timeBlockEl) {
     else if (currTime === blockTime) {
         var relBlockTime = "present"
     }
-    else if (currTime < blockTime){
+    else if (currTime < blockTime) {
         var relBlockTime = "future"
     }
     console.log(relBlockTime);
@@ -60,16 +55,15 @@ var checkTime = function (timeBlockEl) {
 
 $(".container").on("click", "button", function (event) {
     event.preventDefault;
-    
+
     var btnDivEl = $(this).closest("div");
     var textAreaEl = btnDivEl.siblings("textarea");
-    console.log("TEXTAREA",textAreaEl);
 
     var task = textAreaEl.val();
     var taskTime = textAreaEl.attr("id");
     console.log(task, taskTime);
 
-    var timeTask = {"time":taskTime, "task":task};
+    var timeTask = { "time": taskTime, "task": task };
     tasks.push(timeTask);
     saveTasks();
 
@@ -79,5 +73,20 @@ var saveTasks = function () {
     localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+var loadTasks = function () {
+    savedTasks = JSON.parse(localStorage.getItem("tasks"));
 
+    if (!savedTasks) {
+        var savedTasks = [];
+    }
+
+    console.log(savedTasks);
+
+    for (var i = 0; i < savedTasks.length; i++) {
+        var task = savedTasks[i].task;
+        var time = savedTasks[i].time;
+    }
+};
 displayHourChunks();
+
+loadTasks();
